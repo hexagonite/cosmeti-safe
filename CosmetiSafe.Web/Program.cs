8d0e7e3573fd35f1e29449e69d417c71d1992048
Robert Osuch <robosuch@gmail.com>
2023-08-29 09:20:49 +0200

    Added swagger

--- CosmetiSafe.Web/Program.cs
+++ CosmetiSafe.Web/Program.cs
@@ -2,12 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CosmetiSafe API");
+        c.RoutePrefix = "swagger";
+    });
+}
+else
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
