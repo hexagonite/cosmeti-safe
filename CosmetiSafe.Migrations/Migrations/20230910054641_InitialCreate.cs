ced80dc605ec0baf59b16e091bd99c9376c9c4b4
Robert Osuch <robosuch@gmail.com>
2023-09-10 08:34:20 +0200

    Added InitialCreate migration

---
+++ CosmetiSafe.Migrations/Migrations/20230910054641_InitialCreate.cs
@@ -0,0 +1,299 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosmetiSafe.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Concerns",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concerns", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    PrimaryName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HazardScore = table.Column<int>(type: "int", nullable: false),
+                    MinimumHazardScore = table.Column<int>(type: "int", nullable: false),
+                    DataAvailability = table.Column<int>(type: "int", nullable: false),
+                    DependsOnUsageDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    WorstIngredientScore = table.Column<int>(type: "int", nullable: false),
+                    MedianIngredientScore = table.Column<decimal>(type: "decimal(2,1)", nullable: false),
+                    AverageIngredientScore = table.Column<decimal>(type: "decimal(2,1)", nullable: false),
+                    MarketedFor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientConcerns",
+                columns: table => new
+                {
+                    IngredientId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    ConcernId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    ConcernSeverity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientConcerns", x => new { x.IngredientId, x.ConcernId });
+                    table.ForeignKey(
+                        name: "FK_IngredientConcerns_Concerns_ConcernId",
+                        column: x => x.ConcernId,
+                        principalTable: "Concerns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IngredientConcerns_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientContaminants",
+                columns: table => new
+                {
+                    IngredientId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    ContaminantId = table.Column<byte[]>(type: "varbinary(16)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientContaminants", x => new { x.IngredientId, x.ContaminantId });
+                    table.ForeignKey(
+                        name: "FK_IngredientContaminants_Ingredients_ContaminantId",
+                        column: x => x.ContaminantId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_IngredientContaminants_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientSynonyms",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IngredientId = table.Column<byte[]>(type: "varbinary(16)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientSynonyms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientSynonyms_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductIngredients",
+                columns: table => new
+                {
+                    ProductId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    IngredientId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductIngredients", x => new { x.ProductId, x.IngredientId });
+                    table.ForeignKey(
+                        name: "FK_ProductIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductIngredients_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoreProducts",
+                columns: table => new
+                {
+                    ProductId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    StoreId = table.Column<byte[]>(type: "varbinary(16)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(9,2)", nullable: false),
+                    Currency = table.Column<int>(type: "int", nullable: false),
+                    ProductUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreProducts", x => new { x.StoreId, x.ProductId });
+                    table.ForeignKey(
+                        name: "FK_StoreProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoreProducts_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientConcerns_ConcernId",
+                table: "IngredientConcerns",
+                column: "ConcernId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientContaminants_ContaminantId",
+                table: "IngredientContaminants",
+                column: "ContaminantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredients_PrimaryName",
+                table: "Ingredients",
+                column: "PrimaryName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientSynonyms_IngredientId",
+                table: "IngredientSynonyms",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientSynonyms_Name",
+                table: "IngredientSynonyms",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductIngredients_IngredientId",
+                table: "ProductIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreProducts_ProductId",
+                table: "StoreProducts",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_Name",
+                table: "Stores",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientConcerns");
+
+            migrationBuilder.DropTable(
+                name: "IngredientContaminants");
+
+            migrationBuilder.DropTable(
+                name: "IngredientSynonyms");
+
+            migrationBuilder.DropTable(
+                name: "ProductIngredients");
+
+            migrationBuilder.DropTable(
+                name: "StoreProducts");
+
+            migrationBuilder.DropTable(
+                name: "Concerns");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
